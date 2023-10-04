eaeef3641bff2379e29b7a5ee8c79fdec8b0dca9
kevi5050 <146884513+kevi5050@users.noreply.github.com>
2023-10-04 20:02:57 -0300

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -60,7 +60,7 @@ def create_accounts():
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
-@app.route("/accounts", methods=["GET"])
+@app.route("/accounts",  methods=["GET"])
 def list_accounts():
     """
     List all Accounts
